c7a8d6be4d9aa008e1c50e9253aca8a7bdbe9cb8
Unnamedd11 <kuperkuper24@mail.ru>
2026-06-14 16:36:34 +0500

    CallbackScheduler done

--- include/callback_scheduler.h
+++ include/callback_scheduler.h
@@ -1,25 +1,46 @@
 #pragma once
 
 #include <chrono>
-#include <cstdint>
 #include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <set>
+#include <thread>
 
-using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
-
-class CallbackScheduler
-{
+class CallbackScheduler {
 public:
+    using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
     using TaskId = std::uint64_t;
 
-    CallbackScheduler() = default;
-    ~CallbackScheduler() = default;
+    CallbackScheduler();
+    ~CallbackScheduler();
 
     CallbackScheduler(const CallbackScheduler&) = delete;
     CallbackScheduler& operator=(const CallbackScheduler&) = delete;
 
-    CallbackScheduler(CallbackScheduler&&) = delete;
-    CallbackScheduler& operator=(CallbackScheduler&&) = delete;
-
     TaskId Schedule(std::function<void()> callback, TimePoint when);
     bool Cancel(TaskId id);
+
+private:
+    struct Task {
+        TaskId id;
+        std::function<void()> callback;
+        TimePoint when;
+
+        bool operator>(const Task& other) const {
+            return when > other.when;
+        }
+    };
+
+    void WorkerLoop();
+
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::priority_queue<Task, std::vector<Task>, std::greater<Task>> tasks_;
+    std::set<TaskId> cancelled_tasks_;
+
+    TaskId next_id_ = 1;
+    bool stop_ = false;
+    std::thread worker_thread_;
 };
--- src/callback_scheduler.cpp
+++ src/callback_scheduler.cpp
@@ -1,23 +1,46 @@
-#include "callback_scheduler.h"
+#pragma once
 
-CallbackScheduler::TaskId CallbackScheduler::Schedule(std::function<void()> callback, TimePoint when)
-{
-    // TODO(student): реализуйте планирование и выполнение callback в момент when.
-    //
-    // Минимальные требования:
-    // - callback должен быть вызван не раньше when;
-    // - необходимо поддерживать несколько запланированных callback;
-    // - решение должно быть потокобезопасным;
-    // - метод должен вернуть идентификатор задачи для последующей отмены.
-    (void)callback;
-    (void)when;
-    return 0;
-}
+#include <chrono>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <set>
+#include <thread>
 
-bool CallbackScheduler::Cancel(TaskId id)
-{
-    // TODO(student): отмените задачу по идентификатору id.
-    // Возвращайте true, если задача была найдена и отменена, иначе false.
-    (void)id;
-    return false;
-}
+class CallbackScheduler {
+public:
+    using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
+    using TaskId = std::uint64_t;
+
+    CallbackScheduler();
+    ~CallbackScheduler();
+
+    CallbackScheduler(const CallbackScheduler&) = delete;
+    CallbackScheduler& operator=(const CallbackScheduler&) = delete;
+
+    TaskId Schedule(std::function<void()> callback, TimePoint when);
+    bool Cancel(TaskId id);
+
+private:
+    struct Task {
+        TaskId id;
+        std::function<void()> callback;
+        TimePoint when;
+
+        bool operator>(const Task& other) const {
+            return when > other.when;
+        }
+    };
+
+    void WorkerLoop();
+
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    std::priority_queue<Task, std::vector<Task>, std::greater<Task>> tasks_;
+    std::set<TaskId> cancelled_tasks_;
+
+    TaskId next_id_ = 1;
+    bool stop_ = false;
+    std::thread worker_thread_;
+};
